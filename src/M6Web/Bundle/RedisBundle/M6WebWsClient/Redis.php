3ce2a16c140afa75d807c139be4c753403d3d089
Sylvain GUYON <sguyon.externe@m6.fr>
2014-05-21 18:08:44 +0200

    Correction namespace

--- src/M6Web/Bundle/RedisBundle/M6WebWsClient/Redis.php
+++ src/M6Web/Bundle/RedisBundle/M6WebWsClient/Redis.php
@@ -2,7 +2,7 @@
 
 namespace M6Web\Bundle\RedisBundle\M6WebWsClient;
 
-use M6Web\Bundle\AwsBundle\Cache\CacheInterface;
+use M6Web\Bundle\WSClientBundle\Cache\CacheInterface;
 use M6Web\Bundle\RedisBundle\Redis\Redis as BaseRedis;
 
 /**
