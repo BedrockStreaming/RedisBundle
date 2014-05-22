d1a1fc1951a2065963ffea1046895ccd79ee7522
Sylvain GUYON <sguyon.externe@m6.fr>
2014-05-22 10:50:28 +0200

    Bad namespace

--- src/M6Web/Bundle/RedisBundle/M6WebAws/Redis.php
+++ src/M6Web/Bundle/RedisBundle/M6WebAws/Redis.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace M6Web\Bundle\RedisBundle\M6WebWsClient;
+namespace M6Web\Bundle\RedisBundle\M6WebAws;
 
 use M6Web\Bundle\AwsBundle\Cache\CacheInterface;
 use M6Web\Bundle\RedisBundle\Redis\Redis as BaseRedis;
