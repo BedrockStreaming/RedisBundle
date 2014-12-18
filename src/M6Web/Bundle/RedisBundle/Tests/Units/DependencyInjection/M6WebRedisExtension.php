33f02723a3fec78e73c6a7a529fce90c2ba4daaf
Olivier Mansour <olivier.mansour@m6.fr>
2014-12-18 11:59:57 +0100

    some notes on unit test

--- src/M6Web/Bundle/RedisBundle/Tests/Units/DependencyInjection/M6WebRedisExtension.php
+++ src/M6Web/Bundle/RedisBundle/Tests/Units/DependencyInjection/M6WebRedisExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use M6Web\Bundle\RedisBundle\DependencyInjection\M6WebRedisExtension as BaseM6WebRedisExtension;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use M6Web\Bundle\RedisBundle\EventDispatcher\RedisEvent;
 
 
 class M6WebRedisExtension extends atoum\test
@@ -70,6 +71,36 @@ class M6WebRedisExtension extends atoum\test
                     ->isEqualTo('server1');
     }
 
+    // test not working
+//    public function testDataCollector()
+//    {
+//        $this->initContainer();
+//        $this->loadConfiguration($this->container, 'basic_config');
+//        $this->container->compile();
+//
+//        $this->assert
+//                ->object($dataCollector = $this->container->get('m6.data_collector.redis'))
+//                    ->isInstanceOf('M6Web\Bundle\RedisBundle\DataCollector\RedisDataCollector');
+//
+//        $event = new RedisEvent();
+//        $event->setCommand('one_command');
+//        $event->setExecutionTime(0.02);
+//        $event->setArguments(['foo', 'bar']);
+//        $event->setClientName('redis.command');
+//
+//        $this->if($eventDispatcher = $this->container->get('event_dispatcher'))
+//            ->then($eventDispatcher->dispatch('redis.command', $event))
+//            ->object($this->container->get('m6.data_collector.redis')->getCommands())
+//                ->isInstanceOf('\SplQueue')
+//            ->integer(count($dataCollector->getCommands()))
+//                ->isEqualto(1)
+//        ;
+//        var_dump($this->container->get('m6.data_collector.redis')->getCommands());
+//        // TODO
+//
+//
+//    }
+
     public function test2serverConfiguration()
     {
         $this->initContainer();
@@ -114,5 +145,14 @@ class M6WebRedisExtension extends atoum\test
                     ->isEqualTo(1)
                 ->string($serviceRedis->getOptions()->prefix->getPrefix())
                     ->isEqualTo('server1\\');
+
+
+        // this demonstrate that the consistent hashing isnt fully functionnal
+        // https://github.com/nrk/predis/issues/225
+//        $faker = Faker\Factory::create();
+        // test sharding
+//        for ($i=0;$i<=1000;$i++) {
+//            $this->container->get('m6_redis.all')->set('coucou', 'coucou', 20);
+//        }
     }
 } 
