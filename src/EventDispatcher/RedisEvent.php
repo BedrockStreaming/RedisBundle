3b4f56880106e0e7f5729ad5e9fd20d2bf9cc707
Romuald_Priol <romuald.priol@ext.euronews.com>
2021-08-10 09:57:12 +0200

    feat(sf5): update events

--- src/EventDispatcher/RedisEvent.php
+++ src/EventDispatcher/RedisEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace M6Web\Bundle\RedisBundle\EventDispatcher;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Redis event
